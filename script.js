1d2728dbc2670e372ca6767899718c4a8af5fc8e
Isaac Flores <isaacfloresva@gmail.com>
2025-01-14 15:54:45 -0700

    Access to element iframe

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('navbar').addEventListener(
+document.getElementById("navbar").addEventListener(
   "click",
   function (event) {
     console.log("Mensaje recibido:", event.data);
@@ -9,6 +9,26 @@ document.getElementById('navbar').addEventListener(
     } else if (event.data.target === "section3") {
       document.getElementById("section3").scrollIntoView();
     }
+    accederIframe();
   },
   false
 );
+
+function accederIframe() {
+  // Asegúrate de que el iframe haya cargado completamente
+  var ifnavbar = document.getElementById("navbar");
+  var ifnavbarDocument = ifnavbar.contentDocument || ifnavbar.contentWindow.document;
+
+  // Accede y modifica elementos dentro del iframe
+  var elemento = ifnavbarDocument.getElementById("menu-button")
+  if (elemento) {
+    elemento.addEventListener("click", function (){
+      if(ifnavbar.style.height === "250px"){
+      ifnavbar.style.height = "";
+      }else {ifnavbar.style.height = "250px"}
+  });
+  
+    
+    //
+  }
+}
